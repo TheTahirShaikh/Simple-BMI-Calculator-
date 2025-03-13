d4182b70e31bc51db6cdd3f3f7e7eaf4f18d34d6
Tahir Javed Shaikh <111053600+TheTahirShaikh@users.noreply.github.com>
2025-03-13 20:24:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+# I have build simple program which calculates your body mass index
+height = float(input("Enter your height in meter :"))
+weight = int(input("Enter your weight in kilograms :"))
+bmi = weight / height**2
+print("Your BODY MASS INDEX is :", bmi)
